86ff24a0c7c3f73a8ab8025b0d31ac8e65a9ef99
kumastry <chma19063@g.nihon-u.ac.jp>
2022-12-31 18:32:50 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	const clickHandle = () => {
+		alert('helo');
+	};
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={clickHandle}> kuricck </button>
